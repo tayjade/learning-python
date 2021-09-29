78a7d2a4adb5bdd28491614d296ec5a952bcd6a1
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2021-09-28 23:48:36 -0600

    Update Divisible_by_5.py

--- Divisible_by_5.py
+++ Divisible_by_5.py
@@ -5,3 +5,19 @@ if n % 5 == 0:
     print ("yes")
 else:
      print ("no")
+
+'''
+program_running = True 
+
+while program_running:
+    n = input("Please enter a number")
+    n = int(n)
+
+    if n == '':
+        exit() 
+
+    if n % 5 == 0:
+        print ("yes")
+    else:
+        print ("no")
+'''

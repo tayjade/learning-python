563c74f6313e1640f23d9f4afb45f3c5e35c3a1e
tayjade <90885478+tayjade@users.noreply.github.com>
2021-10-05 22:55:27 -0600

    Update list_less_than_ten.py

--- list_less_than_ten.py
+++ list_less_than_ten.py
@@ -78,3 +78,16 @@ What is the data type of a list?
 #  Take a list, say for example this one:
 #  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #  and write a program that prints out all the elements of the list that are less than 5.
+
+
+#  Take a list, say for example this one:
+#  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#  and write a program that prints out all the elements of the list that are less than 5.
+
+list =  [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for n in list:
+    if n < 5:
+        print (n)
+    else:
+        pass 

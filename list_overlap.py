8c17e74047610473fba9939b557a7b940a3ec590
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2021-10-13 19:54:06 -0700

    Update list_overlap.py

--- list_overlap.py
+++ list_overlap.py
@@ -6,3 +6,37 @@ Take two lists, say for example these two:
 and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). 
 Make sure your program works on two lists of different sizes.
 '''
+
+    my_list = ['red, orange, yellow, green, blue, purple, cyan, indigo']
+
+    some_other_list = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'cyan', 'indigo']
+
+    #1st example
+    for item in my_list:
+        if "e" in item:
+            print(item)
+
+    print('\n\n\n\n\n')
+
+    #2nd example
+    for item in some_other_list:
+        if "e" in item:
+            print(item)
+
+
+    #Example output
+    '''
+    red, orange, yellow, green, blue, purple, cyan, indigo
+
+
+
+
+
+
+    red
+    orange
+    yellow
+    green
+    blue
+    purple
+    '''

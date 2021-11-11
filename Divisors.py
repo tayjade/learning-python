274c2c80879dcd99e3511f1ddeae2b7eb42ecaaa
tayjade <90885478+tayjade@users.noreply.github.com>
2021-11-11 16:55:27 -0700

    Update Divisors.py

--- Divisors.py
+++ Divisors.py
@@ -4,3 +4,17 @@ Create a program that asks the user for a number and then prints out a list of a
 it is a number that divides evenly into another number. 
 For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 '''
+n = input("Please enter a number")
+n = int(n)
+
+divisor_list = [] 
+
+number_range = range(1, n+1)
+
+for number in number_range:
+    if n % number == 0:
+        divisor_list.append(number)
+    else:
+        pass
+
+print (divisor_list)
